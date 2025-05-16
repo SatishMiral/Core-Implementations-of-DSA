f7a1c96c2b25787a1fedc6da471ae77dea4956f8
SatishMiral <satishmiral01@gmail.com>
2025-05-16 16:19:16 +0530

    Add HashMap Data Structure

---
+++ Algorithms/SlowFastPointer.java
@@ -0,0 +1,53 @@
+public class SlowFastPointer {
+    public static class LinkedList {
+        public static class Node {
+            int data;
+            Node next;
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public LinkedList() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+
+        public void addFirst(int data) {
+            size++;
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public Node findMid() {
+            Node fast = head, slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        SlowFastPointer.LinkedList.Node mid = ll.findMid();
+        System.out.println("Middle: " + mid.data);
+    }
+}
