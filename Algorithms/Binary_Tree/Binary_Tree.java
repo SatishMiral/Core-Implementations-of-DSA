162142378984b95e2ecc5babd01cf468196734bb
SatishMiral <satishmiral01@gmail.com>
2025-06-19 22:11:33 +0530

    Add Binary Tree Traversal Recursive

---
+++ Algorithms/Binary_Tree/Binary_Tree.java
@@ -0,0 +1,53 @@
+class Binary_Tree {
+    public static class Node{
+        int data;
+        Node left, right;
+        public Node(int item){
+            data = item;
+            left = right = null;
+        }
+    }
+
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        // preorder(root);
+        // inorder(root);
+        postorder(root);
+    }
+}
