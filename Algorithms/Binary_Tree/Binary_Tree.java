902bdbe59b387d7666ce20114f8b1a7bc9aaaf59
SatishMiral <satishmiral01@gmail.com>
2025-06-21 22:44:05 +0530

    Add Binary Tree Traversal Iterative

--- Algorithms/Binary_Tree/Binary_Tree.java
+++ Algorithms/Binary_Tree/Binary_Tree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Binary_Tree {
     public static class Node{
         int data;
@@ -8,6 +10,7 @@ class Binary_Tree {
         }
     }
 
+    // Root Left Right
     public static void preorder(Node root){
         if(root == null){
             return;
@@ -18,6 +21,27 @@ class Binary_Tree {
         preorder(root.right);
     }
 
+    // Initially push root in stack
+    // Use a Stack Print the curr Node.data Push Right first then left while stack is not empty
+    public static void preorderItr(Node root){
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        while (!st.isEmpty()) {
+            Node node = st.pop();
+            nums.add(node.data);
+
+            if(node.right != null) st.push(node.right);
+            if(node.left != null) st.push(node.left);
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            System.out.print(nums.get(i) + " ");
+        }
+    }
+
+    // Left Root Right
     public static void inorder(Node root){
         if(root == null){
             return;
@@ -28,6 +52,35 @@ class Binary_Tree {
         inorder(root.right);
     }
 
+    // Do not initially push root in stack
+    // if a node is not null then push it in stack got to as left as possible
+    // if a node is null then get the top node of stack print it and go to its right if the stack is not empty
+    public static void inorderItr(Node root){
+        Stack<Node> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Node node = root;
+
+        while(true){
+            if(node != null){
+                st.push(node);
+                node = node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node = st.pop();
+                list.add(node.data);
+                node = node.right;
+            }
+        }
+
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+    // Left Right Root
     public static void postorder(Node root){
         if(root == null){
             return;
@@ -36,6 +89,59 @@ class Binary_Tree {
         postorder(root.left);
         postorder(root.right);
         System.out.print(root.data + " ");
+    }
+
+    // Using 2 Stacks
+    // Initially push root in stack1
+    // Pop the stack1 top and push it in stack2
+    // If Left exists push it in stack1, same for right
+    // pop all nodes from stack2 add it in result
+    public static void postorderItr1(Node root){
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        st1.push(root);
+
+        while(!st1.isEmpty()){
+            Node node = st1.pop();
+            st2.push(node);
+
+            if(node.left != null) st1.push(node.left);
+            if(node.right != null) st1.push(node.right);
+        }
+
+        while(!st2.isEmpty()){
+            list.add(st2.pop().data);
+        }
+
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+    // Add the root in Queue
+    // Get the first element in res
+    // if left exist then add it in queue, same for right
+    public static void levelOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            Node node = queue.remove();
+            System.out.print(node.data + " ");
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -48,6 +154,10 @@ class Binary_Tree {
         root.right.right = new Node(7);
         // preorder(root);
         // inorder(root);
-        postorder(root);
+        // postorder(root);
+        // levelOrder(root);
+        // preorderItr(root);
+        // inorderItr(root);
+        postorderItr1(root);
     }
 }
